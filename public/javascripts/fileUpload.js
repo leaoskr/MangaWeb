fe93fc0a0b95b003016310362cf6476b71b456fc
leaoskr <yuqian.shi@yahoo.com>
2022-08-21 16:42:05 -0400

    finish css style

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,30 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-)
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
-FilePond.parse(document.body);
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') !== ''){
+    ready()
+}else{
+    document.getElementById('main-css')
+    .addEventListener('load', ready)
+}
+
+function ready(){
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-width-ratio'))
+    const coverHeight = coverWidth / coverRatio
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    FilePond.parse(document.body);
+}
+
+
+
+
